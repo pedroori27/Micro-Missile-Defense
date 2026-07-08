576cece02b476d583c0461eb287fbf23810b2d8e
PedroOri27 <pedroori27@gmail.com>
2026-07-08 16:15:16 -0300

    Commit

--- teste-pyserial.py
+++ teste-pyserial.py
@@ -1,7 +1,21 @@
 import serial
+import serial.tools.list_ports
 import time
 
-arduino = serial.Serial("/dev/ttyACM0", 9600)
+arduino_porta = None # Define a variável arduino_porta como None
+
+for porta in serial.tools.list_ports.comports(): # procura todas as portas seriais disponíveis
+    print(porta.device, "-", porta.description) 
+
+    if "Arduino" in porta.description: # Verifica se a descrição da porta contém Arduino, ai prova q a porta é do Arduino
+        arduino_porta = porta.device
+        break
+
+if arduino_porta is None: # Caso não encontre a porta do arduino :(
+    print("Arduino não encontrado.")
+    exit()
+
+arduino = serial.Serial(arduino_porta, 9600) # Aqui é onde a comunicação serial é iniciada com o Arduino, usando a porta encontrada e a taxa de transmissão de 9600 bps
 
 time.sleep(10)  # Espera o Arduino reiniciar
 
